f0fbe268962dd7bf215f185c38ee6d40c32d66be
Jared Henderson <jared@netrivet.com>
2021-10-25 08:24:17 -0400

    SAVEPOINT

--- src/cmd/each-yml/handler.ts
+++ src/cmd/each-yml/handler.ts
@@ -1,26 +1,65 @@
 import fs from 'fs';
-import { Friend } from '@friends-library/friends';
-import { allFriends } from '@friends-library/friends/query';
-import env from '@friends-library/env';
 
-export default async function handler(): Promise<void> {
-  const ymls = getYmls();
-  console.log(ymls.length);
-}
+const edition = 'original';
+const docSlug = `piety-promoted-v1`;
+const compDir = `/Users/jared/fl/en/compilations`;
 
-function getYmls(): { friend: Friend; path: string; raw: string; repoPath: string }[] {
-  const { DOCS_REPOS_ROOT, DEV_LIBS_PATH } = env.require(
-    `DOCS_REPOS_ROOT`,
-    `DEV_LIBS_PATH`,
-  );
+export default function handler(): void {
+  const path = `${compDir}/${docSlug}/${edition}/raw.adoc`;
+  const adoc = fs.readFileSync(path).toString();
 
-  return allFriends().map((friend) => {
-    const path = `${DEV_LIBS_PATH}/friends/yml/${friend.path}.yml`;
-    return {
-      friend,
-      path,
-      raw: fs.readFileSync(path).toString(),
-      repoPath: `${DOCS_REPOS_ROOT}/${friend.path}`,
-    };
+  const parts = adoc
+    .replace(/\n\n([A-Z]{3,}( [A-Z]+\.?)? [A-Z]{3,})/g, '~~~$1')
+    .trim()
+    .split('~~~')
+    .slice(1);
+
+  const chapters: [string, string[], number][] = [['', [], 0]];
+  let chapterIdx = 0;
+
+  const namePattern = /([A-Z]{3,}( [A-Z]+\.?)? [A-Z]{3,})/;
+  parts.forEach((part, idx) => {
+    const match = part.match(namePattern);
+    if (!match) {
+      console.log('Part has no name!', idx, part);
+      return;
+    }
+
+    const name = match[0]
+      .toLowerCase()
+      .split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+      .join(' ');
+
+    let chapter = chapters[chapterIdx];
+    let [, , linesInChapter] = chapter;
+
+    if (linesInChapter > MAX_CHAPTER_LINES) {
+      chapterIdx++;
+      chapter = ['', [], 0];
+      chapters[chapterIdx] = chapter;
+    }
+
+    chapter[0] = `${chapter[0]}\n${part}`;
+    chapter[1].push(name);
+    chapter[2] += part.split('\n').length;
+  });
+
+  chapters.forEach(([mainText, names], idx) => {
+    const title = `== Chapter ${idx + 1}`;
+    const cs = ['[.chapter-synopsis]']
+      .concat(names.map((name) => `* ${name}`))
+      .join('\n');
+    let chapterText = `${title}\n\n${cs}\n\n${mainText}`;
+    const lines = chapterText.split('\n\n');
+    if (lines[lines.length - 1].trim() === "[.asterism]\n'''") {
+      chapterText = lines.slice(0, lines.length - 1).join('\n\n');
+    }
+    const fileNum = String(idx + 2).padStart(2, '0');
+    const chNum = String(idx + 1).padStart(2, '0');
+    const path = `${compDir}/${docSlug}/${edition}/${edition}/${fileNum}-chapter-${chNum}.adoc`;
+    fs.writeFileSync(path, chapterText);
   });
 }
+
+const MAX_CHAPTER_LINES = 800;
